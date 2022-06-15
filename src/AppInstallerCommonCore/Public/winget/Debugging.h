e3ea48fca5e35be3891649af7c3559847686a84e
Kriti Mehta <kritimehta@microsoft.com>
2022-06-16 02:13:08 +0530

    Test

--- src/AppInstallerCommonCore/Public/winget/Debugging.h
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-#pragma once
-
-namespace AppInstaller::Debugging
-{
-    // Enables a self initiated minidump on certain process level failures.
-    void EnableSelfInitiatedMinidump();
-}
